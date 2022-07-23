b9153a7d4ea9841620f4bf96d86d76c377a8771a
Lucas <Lucaselorenzohenrique@gmail.com>
2022-07-23 14:48:20 -0300

    Layout index e Cadastro de produto

--- loja-interativa/routes/web.php
+++ loja-interativa/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProdutoController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('produtos.index');
 });
+
+Route::resource('/produtos', ProdutoController::class)->only([
+    'index',
+    'store',
+]);
